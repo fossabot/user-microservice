0dae01b9eecaeb6d8f75271b7c5a97ad97cc20ae
POIGNANT Thomas <Thomas.POIGNANT@nexity.fr>
2019-01-03 17:35:36 +0100

    configure router outside of main

--- main.go
+++ main.go
@@ -1,56 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
-	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
+	log "github.com/sirupsen/logrus"
 	"github.com/thomaspoignant/user-microservice/config"
+	"github.com/thomaspoignant/user-microservice/server"
 )
 
 var initialized = false
 var ginLambda *ginadapter.GinLambda
 
-// Handler to wrap gin to lambda
-func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	if !initialized {
-		router := SetupRouter()
-		ginLambda = ginadapter.New(router)
-		initialized = true
-	}
-
-	// If no name is provided in the HTTP request body, throw an error
-	return ginLambda.Proxy(req)
-}
-
 func main() {
 	//load config file
 	config.LoadConfigFile()
-	fmt.Println(viper.GetString("awsConfig.region"))
-	/*switch runAs := os.Getenv("RUN_AS"); runAs {
+	switch runAs := os.Getenv("RUN_AS"); runAs {
 	case "lambda":
 		log.Info("Run as lambda")
 		lambdaRun()
 	default:
 		log.Info("Run locally")
 		localRun()
-	}*/
+	}
 }
 
 func localRun() {
-	router := SetupRouter()
-	router.Run() // listen and serve on 0.0.0.0:8080
+	server.Init()
 }
 
 func lambdaRun() {
 	lambda.Start(Handler)
 }
 
-// SetupRouter determine what to do for each api calls
-func SetupRouter() *gin.Engine {
-	router := gin.Default()
-	return router
+// Handler to wrap gin to lambda
+func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if !initialized {
+		router := server.SetupRouter()
+		ginLambda = ginadapter.New(router)
+		initialized = true
+	}
+
+	// If no name is provided in the HTTP request body, throw an error
+	return ginLambda.Proxy(req)
 }
